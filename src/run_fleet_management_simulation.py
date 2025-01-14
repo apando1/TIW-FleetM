577f427f0e6cd8c74c0edee333141a34c61de9da
Alex Pando <alexander.pando1@gmail.com>
2025-01-15 00:26:24 +0100

    Additional quotation to explain the code

--- src/run_fleet_management_simulation.py
+++ src/run_fleet_management_simulation.py
@@ -17,6 +17,7 @@ from fleet_management.task_management import TaskManagement
 from fleet_management.task_assignment import TaskAssignment
 from fleet_management.fleet_management import FleetManagement
 
+#logging system to record messages to log file and console
 def setup_logging(log_file_path: str) -> logging.Logger:
     """
     Configure the logging setup.
@@ -32,21 +33,23 @@ def setup_logging(log_file_path: str) -> logging.Logger:
     )
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s') #Fromat of message
     console_handler.setFormatter(formatter)
     logging.getLogger('').addHandler(console_handler)
     logging.getLogger('matplotlib').setLevel(logging.WARNING)
     return logging.getLogger(__name__)
 
+# Loads and manages the configuration data from the JSON files.
 class ConfigManager:
     """
     Class handles loading and managing configurations.
     """
+    # Defining attributes and assigning the information
     def __init__(self, config_paths: Dict[str, str]):
-        self.config_data = self._load_json(config_paths['config'])
-        self.lif_data = self._load_json(config_paths['lif'])
-        self.agents_initialization_data = self._load_json(config_paths['agents_initialization'])
-        self.transportation_tasks_data = self._load_json(config_paths['transportation_tasks'])
+        self.config_data = self._load_json(config_paths['config']) #general config
+        self.lif_data = self._load_json(config_paths['lif']) #graph
+        self.agents_initialization_data = self._load_json(config_paths['agents_initialization']) #agents
+        self.transportation_tasks_data = self._load_json(config_paths['transportation_tasks']) #tasks
 
     @staticmethod
     def _load_json(path: str) -> dict:
@@ -57,9 +60,9 @@ class ConfigManager:
         :return: The JSON data.
         """
         try:
-            with open(path, 'r') as file:
+            with open(path, 'r') as file: #read mode
                 return json.load(file)
-        except FileNotFoundError:
+        except FileNotFoundError: #handling erros
             logging.error(f"File not found: {path}")
             sys.exit(1)
         except json.JSONDecodeError:
@@ -77,13 +80,13 @@ def run_simulation(config_manager: ConfigManager, logging: logging.Logger):
     agent_simulation_path = "src/mobile_robot_simulation/dist/agent_simulation.exe"
     try:
         subprocess.Popen([agent_simulation_path])
-        # Wait for the agent simulation to start.
+        # Wait 10 s for the agent simulation to start.
         time.sleep(10)
     except subprocess.CalledProcessError as e:
         logging.error(f"Failed to execute {agent_simulation_path}: {e}")
         sys.exit(1)
 
-    # Initialize the objects of the fleet manager.
+    # Initialize the objects/components of the fleet manager.
     graph = Graph(lif_data=config_manager.lif_data)
     agents_object = Agents(config_data=config_manager.config_data, logging=logging)
     task_management = TaskManagement()
@@ -93,6 +96,7 @@ def run_simulation(config_manager: ConfigManager, logging: logging.Logger):
     # Run the simulation for the specified time.
     time.sleep(config_manager.config_data["simulation_run_time"])
 
+# Start of the program
 def main():
     """
     Main function to set up and start the simulation.
@@ -114,5 +118,6 @@ def main():
     # Run the simulation.
     run_simulation(config_manager, logging)
 
+# executes only when run directly 
 if __name__ == "__main__":
     main()
