d12d9452eedd1f0600e442026601c46c0ae6d9f1
Alex Pando <alexander.pando1@gmail.com>
2025-01-14 22:18:59 +0100

    Updated graph.py file

--- src/fleet_management/graph.py
+++ src/fleet_management/graph.py
@@ -7,9 +7,63 @@ class Graph:
     # TODO: Model the graph based on the LIF data.
 
     def __init__(self, lif_data):
-        """
-        Initialize the graph based on the data from the LIF file.
+        # Define the dictionaries to store the data: nodes, edges, stations
+        # Initializes the graph by calling _initialize_graph, 
+        # which populates the dictionaries with the LIF data.
+        
+        self.nodes = {}
+        self.edges = {}
+        self.stations = {}
+        self._initialize_graph(lif_data)
+    
+    def initialize_graph(self,lif_data): 
+        #Extracts nodes, edges, and stations from the LIF data
+        
+        layout = lif_data["layouts"][0]  # Assumed a single layout
 
-        :param lif_data: Data from the LIF file.
-        """
-        pass
+        # Process nodes.
+        for node in layout["nodes"]: # iteration through all nodes in the file
+            self.nodes[node["nodeId"]] = { #identifier of the node
+                "position": node["nodePosition"], #x,y coordinates
+                "vehicleTypes": [vt["vehicleTypeId"] for vt in node["vehicleTypeNodeProperties"]] #List of vehicleTypeId 
+                #For loop, in case many types are possible
+            }
+
+        # Process edges.
+        for edge in layout["edges"]:
+            self.edges[edge["edgeId"]] = { #identifier of the edge
+                "start": edge["startNodeId"], # nodes connected
+                "end": edge["endNodeId"], # nodes connected
+                "vehicleTypes": [ #List of vehicleTypeId and whether rotation is allowed
+                    {
+                        "vehicleTypeId": vt["vehicleTypeId"],
+                        "rotationAllowed": vt["rotationAllowed"]
+                    }
+                    for vt in edge["vehicleTypeEdgeProperties"]
+                ] #For loop, in case many types are possible
+            }
+
+        # Process stations.
+        for station in layout["stations"]: 
+            self.stations[station["stationId"]] = { #identifier of the station
+                "interactionNodes": station["interactionNodeIds"], #nodes associated with station
+                "position": station["stationPosition"] # x, y coordinates and rotation angle (theta) of station
+            }
+
+    def get_neighbors(self, node_id): 
+        # Identifying all nodes directly connected to every node
+        # Find and return neighboring nodes of a given node
+        
+        neighbors = []
+        for edge in self.edges.values(): # Iterates over all edge dictionaries stored in self.edges
+            if edge["start"] == node_id: # checking if the node is start of end of an edge
+                neighbors.append(edge["end"]) #add the other node of the edge to the neighbors list
+            elif edge["end"] == node_id:
+                neighbors.append(edge["start"])
+        return neighbors # To return the list of neighboring node IDs.
+        
+    def __repr__(self): 
+        # Provide a summary of the graph
+       
+        return f"Graph(nodes={len(self.nodes)}, edges={len(self.edges)}, stations={len(self.stations)})"
+    
